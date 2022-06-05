7a83266e100814a600838f6b789362c6abd2e5f7
PaulinaMazuraVeiga <101908881+PaulinaMazuraVeiga@users.noreply.github.com>
2022-06-05 18:11:53 -0300

    Create Hostal.cpp

---
+++ Hostal.cpp
@@ -0,0 +1,22 @@
+#include ../include/Hostal.hpp
+
+
+Hostal(nombre string, direccion string, telefono int,0) ; //empieza con calificacionPromedio=0.
+
+//Hostal() ; es el destructor que no encontré para poner el símbolo del ñoqui.
+
+string getNombre() ;
+
+string getDireccion() ;
+
+int getTelefono() ;
+
+int getCalificacionPromedio() ;
+
+void setNombre(string nom) ;
+
+void setDireccion(string direc) ;
+
+void setTelefono(int tel) ;
+
+void setCalPromedio(int calProm) ;
