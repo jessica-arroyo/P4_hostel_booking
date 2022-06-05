520b95bccd83b44445c588cc4755b79f6cb379cb
PaulinaMazuraVeiga <101908881+PaulinaMazuraVeiga@users.noreply.github.com>
2022-06-05 17:44:30 -0300

    Create DtHostal.cpp

---
+++ DtHostal.cpp
@@ -0,0 +1,15 @@
+#include ../include/DtHostal.hpp
+
+
+
+DtHostal(string nom, string direc, int tel, string calProm) ;
+
+//DtHostal() el destructor. No encuentro el símbolo de ñoqui para ponerle.
+
+string getNombre() ;
+
+string getDireccion() ;
+
+int getTelefono();
+
+int getCalPromedio() ;
