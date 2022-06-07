182fc68e4b3c8db3d7ae7fb92fd4101dd2d5658e
PaulinaMazuraVeiga <101908881+PaulinaMazuraVeiga@users.noreply.github.com>
2022-06-07 20:40:27 -0300

    Update IHostal.hpp

--- IHostal.hpp
+++ IHostal.hpp
@@ -12,17 +12,17 @@ class IHostal {
   public:
     IHostal(){}
     virtual ~IHostal(){}=0 ;
-    virtual altaHostal(DtHostal hostal)=0 ;
-    virtual std::map<string , DtHostal *> hos listarHostales()=0 ; //La colección de Hostales va a ser un map o un set (porque podríamos querer tener ordenados 
+    virtual void altaHostal(DtHostal hostal)=0 ;
+    virtual void std::map<string , DtHostal *> listarHostales()=0 ; //La colección de Hostales va a ser un map o un set (porque podríamos querer tener ordenados 
     //a los Hostales en base a su calificacionPromedio para tener más fácil lo de Top 3 Hostales)
-    virtual map<int , DtHabitacion *> habs listarHabitaciones(DtFechaHora checkIn, DtFechaHora checkOut, bool esGrupal, string nombreHostal)=0 ;
-    virtual ingresarDatos(string nombreHostal, DtHabitacion habitacion)=0 ;
-    virtual agregarHabitacion()=0 ;
-    virtual cancelarHabitacion()=0 ;
-    virtual std::map<string , DtHostal *> hos top3hostales()=0 ;
+    virtual map<int , DtHabitacion *> listarHabitaciones(DtFechaHora checkIn, DtFechaHora checkOut, bool esGrupal, string nombreHostal)=0 ;
+    virtual void ingresarDatos(string nombreHostal, DtHabitacion habitacion)=0 ;
+    virtual void agregarHabitacion()=0 ;
+    virtual void cancelarHabitacion()=0 ;
+    virtual std::map<string , DtHostal *> top3hostales()=0 ;
     virtual DtHostal infoHostal(string nombreHostal)=0 ;
     virtual map<int , DtHabitacion *> habs listarHabitaciones()=0 ;
-    virtual Hostal encontrarHostal(string nombreHostal)=0 ; //acá creo que tiene que devolver el DtHostal. Porque no se pueden devolver objetos.
+    virtual DtHostal encontrarHostal(string nombreHostal)=0 ; //acá puse que se devuelva el DtHostal. Porque no se pueden devolver objetos.
     virtual map<int , DtHabitacion *> habs devolverHabitacionesDisponibles(DtFechaHora checkin, DtFechaHora checkout)=0 ;
 };
 
