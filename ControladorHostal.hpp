7ddfe325cd72b28d22339ffe831ab3876526da9f
PaulinaMazuraVeiga <101908881+PaulinaMazuraVeiga@users.noreply.github.com>
2022-06-07 20:36:42 -0300

    Update ControladorHostal.hpp

--- ControladorHostal.hpp
+++ ControladorHostal.hpp
@@ -17,17 +17,17 @@ class ControladorHostal: public: IHostal {
         ~ControladorHostal() ;
         static ControladorHostal *getInstancia() ;
     
-        altaHostal(DtHostal hostal) ;
+        void altaHostal(DtHostal hostal) ;
         std::map<string , DtHostal *> hos listarHostales() ; //La colección de Hostales va a ser un map o un set (porque podríamos querer tener ordenados 
         //a los Hostales en base a su calificacionPromedio para tener más fácil lo de Top 3 Hostales)
         map<int , DtHabitacion *> habs listarHabitaciones(DtFechaHora checkIn, DtFechaHora checkOut, bool esGrupal, string nombreHostal) ;
-        ingresarDatos(string nombreHostal, DtHabitacion habitacion) ;
-        agregarHabitacion() ;
-        cancelarHabitacion() ;
-        std::map<string , DtHostal *> hos top3hostales() ;
+        void ingresarDatos(string nombreHostal, DtHabitacion habitacion) ;
+        void agregarHabitacion() ;
+        void cancelarHabitacion() ;
+        void std::map<string , DtHostal *> hos top3hostales() ;
         DtHostal infoHostal(string nombreHostal) ;
         map<int , DtHabitacion *> habs listarHabitaciones() ;
-        Hostal encontrarHostal(string nombreHostal) ; //acá creo que tiene que devolver el DtHostal. Porque no se devuelven objetos.
+        DtHostal encontrarHostal(string nombreHostal) ; //acá puse que se devuelva el DtHostal. Porque no se devuelven objetos.
         map<int , DtHabitacion *> habs devolverHabitacionesDisponibles(DtFechaHora checkin, DtFechaHora checkout) ;
         
         
