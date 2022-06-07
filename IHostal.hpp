ad9aa9ca980b2f0f6ed83274726b3c1bf88b1c0f
PaulinaMazuraVeiga <101908881+PaulinaMazuraVeiga@users.noreply.github.com>
2022-06-07 18:11:02 -0300

    Update IHostal.hpp

--- IHostal.hpp
+++ IHostal.hpp
@@ -6,18 +6,20 @@
 #include "DtHabitacion.hpp"
 //tengo que poner include "Habitacion.hpp"?
 #include <map>
+#include <iostream>
 
 class IHostal {
   public:
     altaHostal(DtHostal hostal) ;
-    listarHostales() : Set(DtHostal) ;
-    listarHabitaciones(DtFechaHora checkIn, DtFechaHora checkOut, bool esGrupal, string nombreHostal) : Set(DtHabitacion) ;
+    std::map<string , DtHostal *> hos listarHostales() : Set(DtHostal) ; //La colección de Hostales va a ser un map o un set (porque podríamos querer tener ordenados 
+    //a los Hostales en base a su calificacionPromedio para tener más fácil lo de Top 3 Hostales)
+    map<int , DtHabitacion *> habs listarHabitaciones(DtFechaHora checkIn, DtFechaHora checkOut, bool esGrupal, string nombreHostal) ;
     ingresarDatos(string nombreHostal, DtHabitacion habitacion) ;
     agregarHabitacion() ;
     cancelarHabitacion() ;
-    top3hostales() :  Set(DtHostal) ;
-    infoHostal(string nombreHostal) : DtHostal ;
-    listarHabitaciones(): Set(DtHabitacion) ;
+    std::map<string , DtHostal *> hos top3hostales() ;
+    DtHostal infoHostal(string nombreHostal) ;
+    map<int , DtHabitacion *> habs listarHabitaciones() ;
     Hostal encontrarHostal(string nombreHostal) ; //acá creo que tiene que devolver el DtHostal.
-    devolverHabitacionesDisponibles(DtFechaHora checkin, DtFechaHora checkout): Set(DtHabitacion) ;
+    map<int , DtHabitacion *> habs devolverHabitacionesDisponibles(DtFechaHora checkin, DtFechaHora checkout) ;
 };
