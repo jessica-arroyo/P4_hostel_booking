6caf8b85d396b7f4cf4aaebdd00141a63a57c149
PaulinaMazuraVeiga <101908881+PaulinaMazuraVeiga@users.noreply.github.com>
2022-06-07 17:34:33 -0300

    Update Habitacion.cpp

--- Habitacion.cpp
+++ Habitacion.cpp
@@ -34,3 +34,19 @@ void Habitacion::setPrecio(int precio){
 void Habitacion::setCapacidad(int capacidad){
     this->capacidad = capacidad ;
 }
+
+/*
+//Habitacion::create(DtHabitacion habitacion); //el constructor sería este, el de arriba, o ambos?
+Habitacion::add(Habitacion hab){}
+Habitacion::linkeohab(Hostal hostal){}
+DtHabitacion Habitacion::getDtHabitacion(){}
+bool Habitacion::estadisponibleHabitacion(DtFechaHora checkin, DtFechaHora checkout){}
+int Habitacion::getPrecio(Habitacion hab){} // es el mismo de arriba?
+//Habitacion::encontrarHabitacion(int codigoHabitacion, string nombreHostal){}//verificar que esta efectivamente no devuelva nada (por lo de encontrar pero no devuelve nada).
+Habitacion Habitacion::buscarHab(){} //esto debería estar acá o en el Hostal correspondiente? Porque es el Hostal correspondiente el que tiene la colección de Habitaciones.
+//Habitacion::find(int codigoHabitacion){} //en tanto que find, no debería devolver algo? Una Habitacion en este caso.
+//Habitacion::accederHab(int codigoHabitacion){}
+int Habitacion::encontrarPrecio(Habitacion hab){}
+Habitacion::agregarHabitacion(Habitacion hab){}
+//int Habitacion::getCodigoHab(){} // es el mismo de arriba?
+*/
