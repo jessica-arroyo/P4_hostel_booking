a57804b84a729a0422aa8c00cf0d337f582355d7
PaulinaMazuraVeiga <101908881+PaulinaMazuraVeiga@users.noreply.github.com>
2022-06-07 18:19:33 -0300

    Update IHostal.hpp

--- IHostal.hpp
+++ IHostal.hpp
@@ -10,18 +10,18 @@
 
 class IHostal {
   public:
-    IHostal::IHostal(){}
-    virtual IHostal::IHostal(){}=0 ;
-    altaHostal(DtHostal hostal) ;
-    std::map<string , DtHostal *> hos listarHostales() : Set(DtHostal) ; //La colección de Hostales va a ser un map o un set (porque podríamos querer tener ordenados 
+    IHostal(){}
+    virtual ~IHostal(){}=0 ;
+    virtual altaHostal(DtHostal hostal)=0 ;
+    virtual std::map<string , DtHostal *> hos listarHostales()=0 ; //La colección de Hostales va a ser un map o un set (porque podríamos querer tener ordenados 
     //a los Hostales en base a su calificacionPromedio para tener más fácil lo de Top 3 Hostales)
-    map<int , DtHabitacion *> habs listarHabitaciones(DtFechaHora checkIn, DtFechaHora checkOut, bool esGrupal, string nombreHostal) ;
-    ingresarDatos(string nombreHostal, DtHabitacion habitacion) ;
-    agregarHabitacion() ;
-    cancelarHabitacion() ;
-    std::map<string , DtHostal *> hos top3hostales() ;
-    DtHostal infoHostal(string nombreHostal) ;
-    map<int , DtHabitacion *> habs listarHabitaciones() ;
-    Hostal encontrarHostal(string nombreHostal) ; //acá creo que tiene que devolver el DtHostal.
-    map<int , DtHabitacion *> habs devolverHabitacionesDisponibles(DtFechaHora checkin, DtFechaHora checkout) ;
+    virtual map<int , DtHabitacion *> habs listarHabitaciones(DtFechaHora checkIn, DtFechaHora checkOut, bool esGrupal, string nombreHostal)=0 ;
+    virtual ingresarDatos(string nombreHostal, DtHabitacion habitacion)=0 ;
+    virtual agregarHabitacion()=0 ;
+    virtual cancelarHabitacion()=0 ;
+    virtual std::map<string , DtHostal *> hos top3hostales()=0 ;
+    virtual DtHostal infoHostal(string nombreHostal)=0 ;
+    virtual map<int , DtHabitacion *> habs listarHabitaciones()=0 ;
+    virtual Hostal encontrarHostal(string nombreHostal)=0 ; //acá creo que tiene que devolver el DtHostal.
+    virtual map<int , DtHabitacion *> habs devolverHabitacionesDisponibles(DtFechaHora checkin, DtFechaHora checkout)=0 ;
 };
