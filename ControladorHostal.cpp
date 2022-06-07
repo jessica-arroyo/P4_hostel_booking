b79b8647709261943bd366b8345c425ca87d3a1d
PaulinaMazuraVeiga <101908881+PaulinaMazuraVeiga@users.noreply.github.com>
2022-06-07 20:34:31 -0300

    Update ControladorHostal.cpp

--- ControladorHostal.cpp
+++ ControladorHostal.cpp
@@ -18,6 +18,20 @@ ControladorHostal *ControladorHostal::getInstancia(){
     return _instancia ;
 }
 
+ ControladorHostal::altaHostal(DtHostal hostal){}
+        std::map<string , DtHostal *> hos listarHostales() ; //La colección de Hostales va a ser un map o un set (porque podríamos querer tener ordenados 
+        //a los Hostales en base a su calificacionPromedio para tener más fácil lo de Top 3 Hostales)
+        map<int , DtHabitacion *> habs listarHabitaciones(DtFechaHora checkIn, DtFechaHora checkOut, bool esGrupal, string nombreHostal) ;
+        ingresarDatos(string nombreHostal, DtHabitacion habitacion) ;
+        agregarHabitacion() ;
+        cancelarHabitacion() ;
+        std::map<string , DtHostal *> hos top3hostales() ;
+        DtHostal infoHostal(string nombreHostal) ;
+        map<int , DtHabitacion *> habs listarHabitaciones() ;
+        Hostal encontrarHostal(string nombreHostal) ; //acá creo que tiene que devolver el DtHostal. Porque no se devuelven objetos.
+        map<int , DtHabitacion *> habs devolverHabitacionesDisponibles(DtFechaHora checkin, DtFechaHora checkout) ;
+        
+
 
 
 
