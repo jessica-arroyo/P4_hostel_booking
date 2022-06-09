6769ae4b58b6a05fca8fce8d7f5488c042e87e14
PaulinaMazuraVeiga <101908881+PaulinaMazuraVeiga@users.noreply.github.com>
2022-06-08 21:47:49 -0300

    Update altaHostal.cpp

--- altaHostal.cpp
+++ altaHostal.cpp
@@ -1,17 +1,24 @@
 void altaHostal(DtHostal hostal){// es un IHostal* iHostal el parámetro.
     string nomh = hostal->getNombre() ;
-    map<string, Hostal*>::iterator h = SetHostales.find(nomh) ; 
-    if(h!= SetHostales.end()){
-        throw exception "Hostal ya registrado" ;
+    //le pide el nombre al usuario. Corregir eso.
+     ;
+    //esto iria en bool existeHostal(std::string nomH) 
+    //map<string, Hostal*>::iterator h = SetHostales.find(nomh) ; 
+    //if(h!= SetHostales.end()){
+        //throw exception "Hostal ya registrado" ;
+    //}
+    if(iHostal->existeHostal(std::string nomH)==true){
+         throw exception "Hostal ya registrado" ;
     }
     else{
+        //le pide los otros datos.Corregir eso.
         string direc = hostal->getDireccion() ;
         int tel = hostal->getTelefono() ;
         int calProm = hostal->getCalificacionPromedio() ;
         
-      iHostal->crearAsignatura(nomh,direc,tel,calProm) ; //en esta función se crea la asignatura en el ControladorAsignatura 
-      //y se la guarda en un puntero a una asignatura llamado asignaturacreada.
-      iHostal->add() //Luego en esta función, también en ControladorAsignatura, se añade asignaturacreada a la coleccion de Asignaturas.
+      iHostal->crearHostal(nomh,direc,tel,calProm) ; //en esta función se crea el Hostal en el ControladorHostal. 
+      //y se la guarda en un puntero a un hostal llamado hostalcreado.
+      iHostal->add() //Luego en esta función, también en ControladorHostal, se añade hostalcreado a la coleccion de Hostales.
         
     }
           
