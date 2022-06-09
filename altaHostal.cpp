7b48d062d86f6f929bbfc12ee272d42609e96c07
PaulinaMazuraVeiga <101908881+PaulinaMazuraVeiga@users.noreply.github.com>
2022-06-08 22:26:53 -0300

    Update altaHostal.cpp

--- altaHostal.cpp
+++ altaHostal.cpp
@@ -1,24 +1,30 @@
-void altaHostal(DtHostal hostal){// es un IHostal* iHostal el parámetro.
-    string nomh = hostal->getNombre() ;
+void altaHostal(IHostal* iHostal){// es un IHostal* iHostal el parámetro.
+   
     //le pide el nombre al usuario. Corregir eso.
-     ;
+     string nomh ;
+     cout<<"Ingrese el nombre del Hostal a crear" ;
+     cin>> nomh ;
     //esto iria en bool existeHostal(std::string nomH) 
-    //map<string, Hostal*>::iterator h = SetHostales.find(nomh) ; 
-    //if(h!= SetHostales.end()){
-        //throw exception "Hostal ya registrado" ;
-    //}
+    //return (SetHostales.find(nomh) != SetHostales.end()) ;
     if(iHostal->existeHostal(std::string nomH)==true){
-         throw exception "Hostal ya registrado" ;
+         cout<<"Hostal ya registrado");
+         cin>>nomh ;
     }
     else{
-        //le pide los otros datos.Corregir eso.
-        string direc = hostal->getDireccion() ;
-        int tel = hostal->getTelefono() ;
-        int calProm = hostal->getCalificacionPromedio() ;
-        
-      iHostal->crearHostal(nomh,direc,tel,calProm) ; //en esta función se crea el Hostal en el ControladorHostal. 
+         cout<<"Ingrese la direccion" ;
+         string direc ;
+         cin>> direc ;
+         cout<<"Ingrese el telefono" ;
+         int tel ;
+         cin>> tel ;
+         int calProm = 0 ;
+         iHostal->crearHostal(nomh,direc,tel,calProm) ; //en esta función se crea el Hostal en el ControladorHostal. 
       //y se la guarda en un puntero a un hostal llamado hostalcreado.
-      iHostal->add() //Luego en esta función, también en ControladorHostal, se añade hostalcreado a la coleccion de Hostales.
+      //Así: this->hostalCreado = new Hostal(nomh,direc,tel,calProm);
+      //Poner que ControladorHostal tenga en private un campo Hostal* hostalCreado ;
+      iHostal->addHostal() //Luego en esta función, también en ControladorHostal, se añade hostalcreado a la coleccion de Hostales.
+      //Así: this->SetHostales.insert(make_pair(hostalCreado->getNombre(), hostalCreado));
+	         //this->hostalCreado = NULL;
         
     }
           
