c87d2ade4876954f43a1d6abfe4c2faf908bdf57
Jessikitaa <101907130+Jessikitaa@users.noreply.github.com>
2022-06-10 22:24:57 -0300

    Create IUsuario.hpp

---
+++ IUsuario.hpp
@@ -0,0 +1,34 @@
+#ifndef IUSUARIO_H
+#define IUSUARIO_H
+
+#include <map>
+#include <string>
+#include <set>
+#include <iostream>
+
+class IUsuario {
+  public:
+    IUsuario(){}
+    virtual ~IUsuario(){}=0 ;
+    
+    // alta de usuario 
+    virtual void ingresarEmpleado(DtEmpleado *dte) = 0;
+    virtual void ingresarHuesped(DtHuesped *dth) = 0;
+    virtual void confirmarAlta() = 0;
+    virtual void cancelarAlta() = 0;
+    virtual void reingresarEmail(string emailUser) = 0;
+  
+    virtual map<string, Empleado *> listarEmpleadosNoAsignados(string nombreHostal);
+    virtual void asignarEmpleado(emailEmpleado: string, cargo: TipoCargo) = 0;
+    virtual void cancelarAsignarEmpleado() = 0;
+    virtual map<string, Huesped *> listarHuespedes(int codigoHabitacion);
+    virtual void confirmarHuesped(string emailHuespedReserva) = 0;
+    virtual void agregarHuesped(string emailHuesped) = 0;
+    virtual map<string, Usuario *> listarUsuarios();
+    virtual DtEmpleado mostrarEmpleado(DtEmpleado empleado);  
+    virtual DtHuesped mostrarHuesped(DtHuesped huesped); 
+    virtual void encontrarHuesped(emailHuespedReserva: string);
+      
+};
+
+#endif
