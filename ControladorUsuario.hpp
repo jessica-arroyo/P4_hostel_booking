17e7080588cd6de195c578e3e83d61923bc154f4
Jessikitaa <101907130+Jessikitaa@users.noreply.github.com>
2022-06-11 11:44:39 -0300

    Create ControladorUsuario.hpp

---
+++ ControladorUsuario.hpp
@@ -0,0 +1,44 @@
+#ifndef CONTROLADORUSUARIO_H
+#define CONTROLADORUSUARIO_H
+
+
+#include <map>
+#include <iostream>
+using namespace std;
+
+class ControladorUsuario: public: IUsuario {
+  private:
+    ControladorUsuario() ;
+    static ControladorUsuario *instancia ;
+  
+  public:
+    ~ControladorUsuario() ;
+    static ControladoUsuario *getInstancia();
+    
+    // alta de usuario 
+    void ingresarEmpleado(DtEmpleado *dte);
+    void ingresarHuesped(DtHuesped *dth);
+    void confirmarAlta();
+    void cancelarAlta();
+    void reingresarEmail(string emailUser);  
+      
+    map<string, Empleado *> listarEmpleadosNoAsignados(string nombreHostal);
+    void asignarEmpleado(string emailEmpleado, TipoCargo cargo);
+    void cancelarAsignarEmpleado();
+    map<string, Huesped *> listarHuespedes(int codigoHabitacion);
+    void confirmarHuesped(string emailHuespedReserva);
+    void agregarHuesped(string emailHuesped);
+    map<string, Usuario *> listarUsuarios();
+    DtEmpleado mostrarEmpleado(DtEmpleado empleado);  
+    DtHuesped mostrarHuesped(DtHuesped huesped); 
+    void encontrarHuesped(string emailHuespedReserva);
+    
+    map<string, Reserva *> listarReservas(string nombreHostal, string email);
+        
+  };
+
+
+
+
+
+#endif
