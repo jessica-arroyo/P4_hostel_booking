f6343eed7ef670bff6d82b467ab0922ebf37c6bd
Jessikitaa <101907130+Jessikitaa@users.noreply.github.com>
2022-06-13 01:46:59 -0300

    Update DtEmpleado.cpp

--- src/DtEmpleado.cpp
+++ src/DtEmpleado.cpp
@@ -6,8 +6,9 @@ TipoCargo  DtEmpleado::getCargo() {
     return cargo;
 }
 
-DtEmpleado::DtEmpleado(string nombre, string email, string password, TipoCargo cargo) : DtUsuario(nombre, email, password) {
+DtEmpleado::DtEmpleado(string nombre, string email, string password, TipoCargo cargo, string nombreHostal) : DtUsuario(nombre, email, password) {
     this->cargo = cargo;
+    this->nombreHostal = nombreHostal;
 }
 
 DtEmpleado::~DtEmpleado(){}
